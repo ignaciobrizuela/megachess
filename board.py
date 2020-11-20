146b82c4dc3415fa3f76dd9467d20c3303f1b710
ignaciobrizuela <brzignacio@gmail.com>
2020-11-20 15:44:18 -0300

    Extract the correct pieces from board

--- board.py
+++ board.py
@@ -35,29 +35,29 @@ class Board():
                 if piece == 'p':
                     self.black_pawns.append(pieces.Pawn(piece, 'black', row, col))
                 elif piece == 'h':
-                    self.black_horses.append(pieces.Piece(piece, 'black', row, col))
+                    self.black_horses.append(pieces.Horse(piece, 'black', row, col))
                 elif piece == 'b':
-                    self.black_bishops.append(pieces.Piece(piece, 'black', row, col))
+                    self.black_bishops.append(pieces.Bishop(piece, 'black', row, col))
                 elif piece == 'r':
-                    self.black_rooks.append(pieces.Piece(piece, 'black', row, col))
+                    self.black_rooks.append(pieces.Rook(piece, 'black', row, col))
                 elif piece == 'q':
-                    self.black_queens.append(pieces.Piece(piece, 'black', row, col))
+                    self.black_queens.append(pieces.Queen(piece, 'black', row, col))
                 elif piece == 'k':
-                    self.black_kings.append(pieces.Piece(piece, 'black', row, col))
+                    self.black_kings.append(pieces.King(piece, 'black', row, col))
 
                 # Creates a list with white pieces
                 elif piece == 'P':
                     self.white_pawns.append(pieces.Pawn(piece, 'white', row, col))
                 elif piece == 'H':
-                    self.white_horses.append(pieces.Piece(piece, 'white', row, col))
+                    self.white_horses.append(pieces.Horse(piece, 'white', row, col))
                 elif piece == 'B':
-                    self.white_bishops.append(pieces.Piece(piece, 'white', row, col))
+                    self.white_bishops.append(pieces.Bishop(piece, 'white', row, col))
                 elif piece == 'R':
-                    self.white_rooks.append(pieces.Piece(piece, 'white', row, col))
+                    self.white_rooks.append(pieces.Rook(piece, 'white', row, col))
                 elif piece == 'Q':
-                    self.white_queens.append(pieces.Piece(piece, 'white', row, col))
+                    self.white_queens.append(pieces.Queen(piece, 'white', row, col))
                 elif piece == 'K':
-                    self.white_kings.append(pieces.Piece(piece, 'white', row, col))
+                    self.white_kings.append(pieces.King(piece, 'white', row, col))
 
                 # Creates a list with empty squares
                 else:
