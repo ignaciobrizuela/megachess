54a2f1a3af0b7cf443efa5cf7b2b0f5f968a537d
ignaciobrizuela <brzignacio@gmail.com>
2020-11-20 15:46:06 -0300

    Check valid move captures, each piece

--- pieces.py
+++ pieces.py
@@ -11,33 +11,80 @@ class Piece():
 
 class Pawn(Piece):
 
-    def valid_move_jump(self):
-        if self.color == 'black':
+    def valid_move_jump(self, direction):
+        if self.color == 'black' and direction == 'down':
             if self.row == 3:
                 return 2
             else:
                 return 1
 
-        elif self.color == 'white':
+        elif self.color == 'white' and direction == 'up':
             if self.row == 12:
                 return -2
             else:
                 return -1
+
+    def valid_move_capture(self, rival_row, rival_col):
+        n_squares_row = rival_row - self.row
+        n_squares_col = abs(rival_col - self.col)
+
+        if self.color == 'black' and n_squares_row == 1 and n_squares_col == 1:
+            return True
+        elif self.color == 'white' and n_squares_row == -1 and n_squares_col == 1:
+            return True
+        else:
+            return None
+
+class Horse(Piece):
+
+    def valid_move_capture(self, rival_row, rival_col):
+        n_squares_row = rival_row - self.row
+        n_squares_col = rival_col - self.col
+
+        # print(n_squares_row, n_squares_col)
+
+        # if n_squares_row > 0 and n_squares_col == 0 or n_squares_row == 0 and n_squares_col >= 1:
+        #     return True
+        # else:
+        #     return None
+
+class Bishop(Piece):
+
+    def valid_move_capture(self, rival_row, rival_col):
+        n_squares_row = abs(rival_row - self.row)
+        n_squares_col = abs(rival_col - self.col)
+
+        if n_squares_row == n_squares_col:
+            return True
+        else:
+            return None
+
+class Rook(Piece):
+
+    def valid_move_capture(self, rival_row, rival_col):
+        n_squares_row = rival_row - self.row
+        n_squares_col = rival_col - self.col
+
+        if n_squares_row > 0 and n_squares_col == 0 or n_squares_row == 0 and n_squares_col >= 1:
+            return True
+        else:
+            return None
 
 class Queen(Piece):
 
-    def valid_move_jump(self):
-        if self.color == 'black':
-            if self.row == 3:
-                return 2
-            else:
-                return 1
+    def valid_move_capture(self, rival_row, rival_col):
+        return True
 
-        elif self.color == 'white':
-            if self.row == 12:
-                return -2
-            else:
-                return -1
+class King(Piece):
+
+    def valid_move_capture(self, rival_row, rival_col):
+        n_squares_row = rival_row - self.row
+        n_squares_col = rival_col - self.col
+
+        if n_squares_row <= 1 and n_squares_col <= 1:
+            return True
+
+        return False
 
 class EmptySquare():
     # Constrains
