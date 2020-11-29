f80ea7a19dfd74d46f52457715e0ed7ecb0470d5
ignaciobrizuela <brzignacio@gmail.com>
2020-11-29 02:10:39 -0300

    Working in the new strategy

--- scripts/player2.py
+++ scripts/player2.py
@@ -5,57 +5,55 @@ from scripts import moves
 from scripts import pieces
 import numpy as np 
 
-play_moves = []
-play = {}
-
-def set_score(piece, rival):
-    print(rival.points)
-    score = rival.points * 10
-    play = {'piece_row': piece.row, 
-            'piece_col': piece.col,
-            'rival_row': rival.row,
-            'rival_col': rival.col,
-            'score': score}
-    play_moves.append(play)
-
-def get_score():
-    return play_moves
 
 def minimax(actual_board, color):
+    rivals = []
     rival = None
 
     for i in range(16):
         for j in range(16):
             piece = actual_board.matrix_pieces[i][j]
             if not isinstance(piece, pieces.EmptySquare):
-                # print('piece', piece.row, piece.col)
-                try:
-                    row, col = moves.rival_up_right(actual_board, piece)
-                    rival = actual_board.matrix_pieces[row][col]
-                    print(rival.row, rival.col)
-                except Exception as e:
-                    rival = None
+                piece.rivals.append(moves.scan_up(actual_board, piece))
+                piece.rivals.append(moves.scan_up_right(actual_board, piece))
+                piece.rivals.append(moves.scan_right(actual_board, piece))
+                piece.rivals.append(moves.scan_down_right(actual_board, piece))
+                piece.rivals.append(moves.scan_down(actual_board, piece))
+                piece.rivals.append(moves.scan_down_left(actual_board, piece))
+                piece.rivals.append(moves.scan_left(actual_board, piece))
+                piece.rivals.append(moves.scan_up_left(actual_board, piece))
 
-            if rival != None:
-                if moves.can_capture(piece, rival):
-                    set_score(piece, rival)
-                elif moves.can_capture(rival, piece):
-                    set_score(rival, piece)
-            
-            rival = None
+                for r in piece.rivals:
+                    print(piece.row, piece.col, r)
+                    try:
+                        row, col = r
+                        rival = actual_board.matrix_pieces[row][col]
+                    except Exception as e:
+                        rival = None
+
+                    if rival != None:
+                        if not isinstance(rival, pieces.EmptySquare):
+                            # If I can capture my oponent piece
+                            if moves.can_capture(piece, rival):
+                                set_score(piece, rival, color)
+                            # If I my oponent can capture my piece
+                            if moves.can_capture(rival, piece):
+                                set_score(rival, piece, color)
+                            rival = None
+                        
 
 
 board1 = (  'rrhhbbqqkkbbhhrr'
             'rrhhbbqqkkbbhhrr'
             ' ppppppppppppppp'
             ' ppppppppppppppp' 
-            '                '
-            '                '
+            '      Q         '
+            '       b        '
             '                '
             ' p              ' 
             'P              q'
             '                '
-            '                '
+            '        Kr      '
             '                ' 
             'PPPPPPPPPPPPPPPP'
             'PPPPPPPPPPPPPPPP'
@@ -71,13 +69,11 @@ def play(actual_board, color):
     scores = get_score()
 
     for s in scores:
-        print(s['score'])
+        print(s)
 
-play(board1, color)
-# actual_board = board.Board(board1)
-# pawn = pieces.Pawn('white', 8, 0)
-# row, col = moves.rival_up_right(actual_board, pawn)
-# rival = actual_board.matrix_pieces[row][col]
-# print(moves.can_capture(pawn, rival))
-# print(moves.can_capture(rival, pawn))
-# print(actual_board.matrix_pieces)
+# play(board1, color)
+
+pawn = pieces.Pawn('white', 12, 1)
+rival = pieces.Pawn('black', 11, 2)
+
+print(pawn.valid_move(rival))
