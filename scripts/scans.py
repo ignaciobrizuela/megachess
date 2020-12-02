e2da5ab5fb5880e1e366d022e65b0deee60f653d
ignaciobrizuela <brzignacio@gmail.com>
2020-12-02 00:11:38 -0300

    Clean script

--- scripts/scans.py
+++ scripts/scans.py
@@ -9,19 +9,10 @@ def scan_up(board, piece):
         yield board.matrix_pieces[row][piece.col]
 
 
-def scan_down(board, piece):
-    for row in range(piece.row+1, 16):
-        yield board.matrix_pieces[row][piece.col]
-
-
-def scan_right(board, piece):
-    for col in range(piece.col+1, 16):
-        yield board.matrix_pieces[piece.row][col]
-
-
-def scan_left(board, piece):
-    for col in reversed(range(0, piece.col)):
-        yield board.matrix_pieces[piece.row][col]
+def scan_L(board, piece):
+    s = ([-2, 1], [-1, 2], [1, 2], [2, 1], [2, -1], [1, -2], [-1, -2], [-2, -1])
+    for i in range(len(s)):
+        yield board.matrix_pieces[piece.row+s[i][0]][piece.col+s[i][1]]
 
 
 def scan_up_right(board, piece):
@@ -30,11 +21,11 @@ def scan_up_right(board, piece):
     for i in range(1, n+1):
         yield board.matrix_pieces[piece.row-i][piece.col+i]
 
-def scan_up_left(board, piece):
-    n = min(piece.row, piece.col)
 
-    for i in range(1, n+1):
-        yield board.matrix_pieces[piece.row-i][piece.col-i]
+def scan_right(board, piece):
+    for col in range(piece.col+1, 16):
+        yield board.matrix_pieces[piece.row][col]
+
 
 def scan_down_right(board, piece):
     n = min(15-piece.row, 15-piece.col)
@@ -43,8 +34,25 @@ def scan_down_right(board, piece):
         yield board.matrix_pieces[piece.row+i][piece.col+i]
 
 
+def scan_down(board, piece):
+    for row in range(piece.row+1, 16):
+        yield board.matrix_pieces[row][piece.col]
+
+
 def scan_down_left(board, piece):
     n = min(15-piece.row, piece.col)
 
     for i in range(1, n+1):
         yield board.matrix_pieces[piece.row+i][piece.col-i]
+
+
+def scan_left(board, piece):
+    for col in reversed(range(0, piece.col)):
+        yield board.matrix_pieces[piece.row][col]
+
+
+def scan_up_left(board, piece):
+    n = min(piece.row, piece.col)
+
+    for i in range(1, n+1):
+        yield board.matrix_pieces[piece.row-i][piece.col-i]
