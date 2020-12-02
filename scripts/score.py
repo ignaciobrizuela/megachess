18c0b146a93a26c1281d03da75830ee468a2bb45
ignaciobrizuela <brzignacio@gmail.com>
2020-12-02 00:12:09 -0300

    Improving move points

--- scripts/score.py
+++ scripts/score.py
@@ -7,23 +7,35 @@ class Score():
 
     def set_score(self, piece, element_in_square, color):
         factor = 1 if piece.color == color else -1
+        score = 0
         
         if isinstance(element_in_square, pieces.EmptySquare):
-            points = piece.points 
-            score = points * factor
-        else:
-            score = element_in_square.points * 10 * factor
+            if isinstance(piece, pieces.Pawn):
+                plus = round(1 / abs(7.5 - element_in_square.row), 2)
+                score = (60 + plus) * factor 
+            else:
+                score = piece.point_move  * factor
+
+        elif piece.color == False and element_in_square.color == True:
+            if element_in_square.row < 11:
+                score = element_in_square.point_been_captured * factor
+            else:
+                score = element_in_square.point_been_captured / 10 * factor
+
+        elif piece.color == True and element_in_square.color == False:
+            if element_in_square.row > 4:
+                score = element_in_square.point_been_captured * factor
+            else:
+                score = element_in_square.point_been_captured / 10 * factor
 
         play = {'from_row': piece.row, 
                 'from_col': piece.col,
                 'to_row': element_in_square.row,
                 'to_col': element_in_square.col,
                 'score': score}
-        self.play_moves.append(play)
-
-    def get_score(self):
-        return self.play_moves
-
+                
+        if play['score'] != 0:
+            self.play_moves.append(play)
 
     def clear_score(self):
         self.play_moves = []
