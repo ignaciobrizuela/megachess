a361a75b277fae3ab6179a786125532c4352c800
ignaciobrizuela <brzignacio@gmail.com>
2020-12-02 00:11:07 -0300

    New strategy but it needs to be rebuild

--- scripts/player2.py
+++ scripts/player2.py
@@ -5,6 +5,8 @@ import numpy as np
 from scripts import pieces
 from scripts import scans
 from scripts import score
+from copy import deepcopy
+import operator
 
 def scan(actual_score, scan_function, piece, color):
     stop = False
@@ -14,25 +16,54 @@ def scan(actual_score, scan_function, piece, color):
             result = next(scan_function)
         except Exception as e:
             break
-
+        
+        
         if isinstance(result, pieces.EmptySquare):
             if piece.valid_move(result):
                 actual_score.set_score(piece, result, color)
-            else:
-                stop = True
-
-        elif result.color != piece.color:
-            if piece.valid_move(result):
-                actual_score.set_score(piece, result, color)
-                stop = True
-            else:
-                stop = True
 
         else:
-            stop = True
+            if isinstance(piece, pieces.Horse):
+                if result.color != piece.color:
+                    if piece.valid_move(result):
+                        actual_score.set_score(piece, result, color)
 
-def minimax(actual_board, color):
-    actual_score = score.Score()
+            elif result.color != piece.color:
+                if piece.valid_move(result):
+                    actual_score.set_score(piece, result, color)
+                    stop = True
+                else:
+                    stop = True
+
+            else:
+                if piece.valid_move(result):
+                    actual_score.set_score(piece, result, color)
+                    stop = True
+                else:
+                    stop = True
+
+
+def evaluate_better_move(actual_board, actual_score, color):
+    move_score = ([])
+    next_score = score.Score()
+
+    for s in actual_score.play_moves:
+        if s['score'] > 0:
+            next_board = deepcopy(actual_board)
+            next_board.matrix_pieces[s['to_row']][s['to_col']] = next_board.matrix_pieces[s['from_row']][s['from_col']]
+            next_board.matrix_pieces[s['from_row']][s['from_col']] = pieces.EmptySquare(s['from_row'],s['from_col'])
+            next_score = scan_posible_moves(next_board, color)
+            
+            max_move_now, _ = minimax(actual_score)
+            max_move, min_move = minimax(next_score)
+            points = s['score'] * 2 + max_move['score'] + min_move['score']
+            move_score.append([s, points])
+
+    return move_score
+
+
+def scan_posible_moves(actual_board, color):
+    new_score = score.Score()
 
     for i in range(16):
         for j in range(16):
@@ -46,71 +77,53 @@ def minimax(actual_board, color):
                 down_left   = scans.scan_down_left(actual_board, piece)
                 left        = scans.scan_left(actual_board, piece)
                 up_left     = scans.scan_up_left(actual_board, piece)
+                L           = scans.scan_L(actual_board, piece)
 
-                scan(actual_score, up, piece, color)
-                scan(actual_score, up_right, piece, color)
-                scan(actual_score, right, piece, color)
-                scan(actual_score, down_right, piece, color)
-                scan(actual_score, down, piece, color)
-                scan(actual_score, down_left, piece, color)
-                scan(actual_score, left, piece, color)
-                scan(actual_score, up_left, piece, color)
+                scan(new_score, up, piece, color)
+                scan(new_score, up_right, piece, color)
+                scan(new_score, right, piece, color)
+                scan(new_score, down_right, piece, color)
+                scan(new_score, down, piece, color)
+                scan(new_score, down_left, piece, color)
+                scan(new_score, left, piece, color)
+                scan(new_score, up_left, piece, color)
+                scan(new_score, L, piece, color)
 
-    for s in actual_score.get_score():
-        print(s)
+    return new_score
+
+
+def minimax(actual_score):
+    scores = [s['score'] for s in actual_score.play_moves]
+    play_max_score = scores.index(max(scores))
+    play_min_score = scores.index(min(scores))
+
+    return  actual_score.play_moves[play_max_score], actual_score.play_moves[play_min_score]
                         
 
 
-board1 = (  'rrhhbbqqkkbbhhrr'
-            'rrhhbbqqkkbbhhrr'
-            ' ppppppppppppppp'
-            ' ppppppppppppppp' 
-            '      Q         '
-            '       b        '
-            '                '
-            ' p        p     ' 
-            'P              q'
-            '      Q   R b   '
-            '         K      '
-            '                ' 
-            'PPPPPPPPPPPPPPPP'
-            'PPPPPPPPPPPPPPPP'
-            'RRHHBBQQKKBBHHRR'
-            'RRHHBBQQKKBBHHRR')
-            
-color = True
+writer = open('game.txt', 'w')
 
 def play(actual_board, color):
+
+    from_row, from_col, to_row, to_col = (0, 0, 0, 0)
     actual_board = board.Board(actual_board)
+    writer.write(str(actual_board.matrix))
+    writer.write('\n')
+
     print(actual_board.matrix)
-    minimax(actual_board, color)
 
-play(board1, color)
+    scores = scan_posible_moves(actual_board, color)
 
-# actual_board = board.Board(board1)
+    max_move, min_move = minimax(scores)
 
-# piece = pieces.Rook(False, 1, 1)
+    moves = evaluate_better_move(actual_board, scores, color)
 
-# up          = scans.scan_up(actual_board, piece)
-# up_right    = scans.scan_up_right(actual_board, piece)
-# right       = scans.scan_right(actual_board, piece)
-# down_right  = scans.scan_down_right(actual_board, piece)
-# down        = scans.scan_down(actual_board, piece)
-# down_left   = scans.scan_down_left(actual_board, piece)
-# left        = scans.scan_left(actual_board, piece)
-# up_left     = scans.scan_up_left(actual_board, piece)
+    better_move = max(moves, key=operator.itemgetter(1))
+    move = better_move[0]
+    print(move)
 
-# actual_score = score.Score()
 
-# scan(actual_score, up, piece, color)
-# scan(actual_score, up_right, piece, color)
-# scan(actual_score, right, piece, color)
-# scan(actual_score, down_right, piece, color)
-# scan(actual_score, down, piece, color)
-# scan(actual_score, down_left, piece, color)
-# scan(actual_score, left, piece, color)
-# scan(actual_score, up_left, piece, color)
+    writer.write(str(move))
+    writer.write('\n')
 
-# for s in actual_score.get_score():
-#     print(s)
-
+    return move['from_row'], move['from_col'], move['to_row'], move['to_col']
