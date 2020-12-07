9a15ae57d0174c73b0c9f8f8adafc6a059a600f4
ignaciobrizuela <brzignacio@gmail.com>
2020-12-06 23:47:15 -0300

    Update tests board

--- tests/test_board.py
+++ tests/test_board.py
@@ -6,14 +6,48 @@ class TestBoard(unittest.TestCase):
     board_game = None
 
     def setUp(self):
-        board_str = ('rrhhbbqqkkbbhhrrrrhhbbqqkkbbhhrrpppppppppppppppppppppppppppppppp' 
-                    '                                                                ' 
-                    '                                                                ' 
-                    'PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPRRHHBBQQKKBBHHRRRRHHBBQQKKBBHHRR')
-        self.board_game = board.Board(board_str)
+        self.board_pretty = ('♜♜♞♞♝♝♛♛♚♚♝♝♞♞♜♜♜♜♞♞♝♝♛♛♚♚♝♝♞♞♜♜♟♟♟♟♟♟♟♟♟♟♟♟♟♟♟♟♟♟♟♟♟♟♟♟♟♟♟♟♟♟♟♟'
+        '                                                                '
+        '                                                                '
+        '♙♙♙♙♙♙♙♙♙♙♙♙♙♙♙♙♙♙♙♙♙♙♙♙♙♙♙♙♙♙♙♙♖♖♘♘♗♗♕♕♔♔♗♗♘♘♖♖♖♖♘♘♗♗♕♕♔♔♗♗♘♘♖♖')
+        self.board_game = board.Board(self.board_pretty)
 
-    def test_shape_board(self):
-        self.assertEqual((16, 16), self.board_game.matrix.shape, "Must be a 16x16 board")
+    def test_transform_pretty_pieces(self):
+        board_str = (  'rrhhbbqqkkbbhhrr'
+            'rrhhbbqqkkbbhhrr'
+            'pppppppppppppppp'
+            'pppppppppppppppp' 
+            '                '
+            '                '
+            '                '
+            '                ' 
+            '                '
+            '                '
+            '                '
+            '                ' 
+            'PPPPPPPPPPPPPPPP'
+            'PPPPPPPPPPPPPPPP'
+            'RRHHBBQQKKBBHHRR'
+            'RRHHBBQQKKBBHHRR')
+
+        board_pieces = board.transform_pretty_pieces(board_str)
+
+        self.assertEqual(8, board_pieces.count('♜'))
+        self.assertEqual(8, board_pieces.count('♞'))
+        self.assertEqual(8, board_pieces.count('♝'))
+        self.assertEqual(4, board_pieces.count('♛'))
+        self.assertEqual(4, board_pieces.count('♚'))
+        self.assertEqual(32, board_pieces.count('♟'))
+        self.assertEqual(8, board_pieces.count('♖'))
+        self.assertEqual(8, board_pieces.count('♘'))
+        self.assertEqual(8, board_pieces.count('♗'))
+        self.assertEqual(4, board_pieces.count('♕'))
+        self.assertEqual(4, board_pieces.count('♔'))
+        self.assertEqual(32, board_pieces.count('♙'))
+    
+    def test_convert_board_matrix(self):
+        matrix_pieces = board.convert_board_matrix(self.board_pretty)
+        self.assertEqual((16, 16), matrix_pieces.shape, "Must be a 16x16 board")
 
     def test_pieces_quantity(self):
         white_pieces = []
